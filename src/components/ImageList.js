ed85753d80c56c069045ed4517c242352b7c2b3d
Zohaib Tariq <zohaib.tariq@arpatech.com>
2019-09-03 12:28:04 +0500

    removed logs

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,6 @@
 import React from 'react'
 class ImageList extends React.Component{
     render = () => {
-        console.log('recieved???')
-        console.log(this.props.images)
         const images = this.props.images.map(({id, urls, description}) => {
             return (
                 <div key={id}>
